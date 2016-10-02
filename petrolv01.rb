a7d21fcb5aedc6488e7f489806bc91d817bef48e
modus <illuminatiday@gmail.com>
2016-10-02 16:17:20 +0300

    #003 Adding csv mode"

--- petrolv01.rb
+++ petrolv01.rb
@@ -3,16 +3,17 @@ require 'digest/md5'
 require 'roo'
 require 'pg'
 require 'zip'
+require 'roo-xls'
 download = open('http://locator.transitcard.ru/locator/web/v1/report/points?pointTypes=8&isStk=false&x1=-58.64249754650558&y1=-30.994994359375003&x2=81.09058618886175&y2=-174.784056859375&zoom=2&type=Xls')
 _cat = Dir.pwd
 name = Time.now
 name = name.to_s
-format = ".xlsx"
+format = ".xls"
 fullname = name + format
 IO.copy_stream(download, fullname)
 puts 'OK Bomb have been planted'
 sleep 2
-list = Dir.glob("*.xlsx") 
+list = Dir.glob("*.xls") 
 puts list
 newfilemd5 = Digest::MD5.hexdigest(File.read(_cat + "/" + list[1]))
 oldfilemd5 = Digest::MD5.hexdigest(File.read(_cat + "/" + list[0]))
@@ -29,10 +30,12 @@ else
 		puts "Old file not valid"
 	
 end
-list = Dir.glob("*.xlsx")
+list = Dir.glob("*.xls")
 puts list
 xlsfile = ("./" + (list[0].to_s))
 puts xlsfile
-xlsx = Roo::Spreadsheet.open(xlsfile, extension: :xlsx)
+xlsx = Roo::Spreadsheet.open(xlsfile)
+puts xlsx
+xlsx =  xlsx.to_csv
 puts xlsx
 
